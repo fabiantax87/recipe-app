86e8c9428067fb7810a293e26f4b51be1d638f9c
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-09-25 14:40:42 +0200

    improved login styling

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { logInWithEmailAndPassword, auth } from "../../firebase/firebase-config";
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./Login.scss";
 
@@ -26,13 +26,41 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <h1>Login</h1>
-      <form className="login-form" onSubmit={(e) => login(e)}>
-        <input type="text" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Log in</button>
-      </form>
-      <Link to={"/register"}>Register instead</Link>
+      <div className="login-content">
+        <h1>Login</h1>
+        <form className="login-form" onSubmit={(e) => login(e)}>
+          <input type="text" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit">Log in</button>
+        </form>
+        <div className="other-options">
+          <button>
+            <svg className="google-icon" viewBox="0 0 48 48">
+              <defs>
+                <path
+                  id="a"
+                  d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                />
+              </defs>
+              <clipPath id="b">
+                <use href="#a" overflow="visible" />
+              </clipPath>
+              <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+              <path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
+              <path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
+              <path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
+            </svg>
+            Sign in with google
+          </button>
+          <button onClick={(e) => navigate("/register")}>
+            <svg className="mail-icon" viewBox="0 0 24 24">
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+              <polyline points="22,6 12,13 2,6"></polyline>
+            </svg>
+            Register instead
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

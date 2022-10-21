dac1a3531f5fa1a800a35fcfc39da6227f9aa523
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-10-22 00:45:47 +0200

    added base form without database link

---
+++ src/components/CreateRecipeModal/CreateRecipeModal.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./CreateRecipeModal.scss";
+
+const CreateRecipeModal = () => {
+  const [groceryList, setGroceryList] = useState<any>([]);
+  const [recipeStepList, setRecipeStepList] = useState<any>([]);
+  const [groceryItem, setGroceryItem] = useState("");
+  const [recipeStep, setRecipeStep] = useState("");
+
+  const addGrocery = (e: any) => {
+    e.preventDefault();
+    setGroceryList([...groceryList, groceryItem]);
+    setGroceryItem("");
+  };
+
+  const addRecipeStep = (e: any) => {
+    e.preventDefault();
+    setRecipeStepList([...recipeStepList, recipeStep]);
+    setRecipeStep("");
+  };
+
+  const submitRecipe = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="modal-background">
+      <div className="modal-container">
+        <div className="modal-header">
+          <h3>Create a recipe</h3>
+        </div>
+        <div className="modal-content">
+          <form onSubmit={(e) => submitRecipe(e)} className="modal-form">
+            <label>Recipe name</label>
+            <input type="text" placeholder="Recipe name" />
+            <label>Meal picture</label>
+            <input type="file" placeholder="Recipe picture" />
+            <div className="grocery-list">
+              {groceryList.map((grocery: any, index: any) => {
+                return (
+                  <div className="grocery-list-item">
+                    <span>{index + 1}.</span>
+                    <p>{grocery}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <label>Grocery list</label>
+            <div className="grocery-list-input">
+              <input type="text" placeholder="Grocery name" value={groceryItem} onChange={(e) => setGroceryItem(e.target.value)} />
+              <button onClick={(e) => addGrocery(e)}>Add</button>
+            </div>
+            <div className="recipe-step-list">
+              {recipeStepList.map((recipeStepItem: any, index: any) => {
+                return (
+                  <div className="recipe-step-item">
+                    <span>{index + 1}.</span>
+                    <p>{recipeStepItem}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <label>Recipe steps</label>
+            <div className="recipe-step-input">
+              <input type="text" placeholder="Step description" value={recipeStep} onChange={(e) => setRecipeStep(e.target.value)} />
+              <button onClick={(e) => addRecipeStep(e)}>Add</button>
+            </div>
+            <button type="submit">Create</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateRecipeModal;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,25 @@
+import CreateRecipeModal from "components/CreateRecipeModal/CreateRecipeModal";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.scss";
 
 const Home = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
   let navigate = useNavigate();
 
   return (
-    <div className="home">
-      <div className="buttons">
-        <button onClick={() => navigate("breakfast")}>Breakfast</button>
-        <button onClick={() => navigate("lunch")}>Lunch</button>
-        <button onClick={() => navigate("dinner")}>Dinner</button>
+    <div className="home-container">
+      {modalOpen ? <CreateRecipeModal /> : <></>}
+      <button className="create-recipe-btn" onClick={(e) => setModalOpen(!modalOpen)}>
+        Create Recipe
+      </button>
+      <div className="home">
+        <div className="buttons">
+          <button onClick={() => navigate("breakfast")}>Breakfast</button>
+          <button onClick={() => navigate("lunch")}>Lunch</button>
+          <button onClick={() => navigate("dinner")}>Dinner</button>
+        </div>
       </div>
     </div>
   );

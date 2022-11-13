8ae287299617b908f2302daabdbefa63b85074a2
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-11-13 16:38:01 +0100

    edited .gitignore

--- src/firebase/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAqr8xDQev0hRjEYTdlqKPyL93HzZ2ovzc",
-  authDomain: "recipe-app-32909.firebaseapp.com",
-  projectId: "recipe-app-32909",
-  storageBucket: "recipe-app-32909.appspot.com",
-  messagingSenderId: "15083685829",
-  appId: "1:15083685829:web:0c0f4121579902914fedac",
-  measurementId: "G-FR3HQRG1PX",
-};
